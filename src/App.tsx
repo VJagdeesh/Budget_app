66c1938f2a8e2efcb1b7fbba8ca2d3de8678a785
unknown <jagdeeshbhavani22@gmail.com>
2024-09-01 00:43:43 +0530

    formatted the fiels

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,40 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
 import Home from "./components/Tabs/Home";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 import awsmobile from "./aws-exports.js";
 // import outputs from '@/amplify_outputs.json';
-import '@aws-amplify/ui-react/styles.css';
+import "@aws-amplify/ui-react/styles.css";
 import Header from "./components/Header/Header.js";
 
 Amplify.configure(awsmobile);
 
 function App() {
-
   const theme = createTheme({
     typography: {
       body2: {
-        fontSize: '1rem', // Default value, adjust as needed
+        fontSize: "1rem", // Default value, adjust as needed
       },
     },
   });
 
   return (
     <>
-    <Authenticator>
-      {({ signOut }) => (
+      <Authenticator>
+        {({ signOut }) => (
           <ThemeProvider theme={theme}>
-          <ChakraProvider>
-          <div className="app">
-            <Header signOut={signOut}/>
-            <div className="tab">
-            <Home />
-            </div>
-          </div>
-          </ChakraProvider>
+            <ChakraProvider>
+              <div className="app">
+                <Header signOut={signOut} />
+                <div className="tab">
+                  <Home />
+                </div>
+              </div>
+            </ChakraProvider>
           </ThemeProvider>
-      )}
-    </Authenticator>
+        )}
+      </Authenticator>
     </>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./Header.css";
 import { Button, ChakraProvider } from "@chakra-ui/react";
-interface HeaderProps{
-   signOut:any; 
+interface HeaderProps {
+  signOut: any;
 }
 
-const Header:React.FC<HeaderProps>=({signOut})=>{
-return(
+const Header: React.FC<HeaderProps> = ({ signOut }) => {
+  return (
     <div className="header">
-        <h1>Welcome to budget tracking app</h1>
-        <Button variant="solid" colorScheme="blue" size="lg" onClick={signOut}>Sign Out</Button>
+      <h1>Welcome to budget tracking app</h1>
+      <Button variant="solid" colorScheme="blue" size="lg" onClick={signOut}>
+        Sign Out
+      </Button>
     </div>
-);
+  );
 };
 
 export default Header;
